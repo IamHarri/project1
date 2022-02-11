883f2f1f5a3abe17f4edb084182944aa848133ca
longlc3 <long18161245@gmail.com>
2022-02-11 15:44:37 +0700

    add

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -8,14 +8,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 
-app = Flask(__name__)
-app.config.from_object(Config)
+# app = Flask(__name__)
+# app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
 # Session(app)
 # db = SQLAlchemy(app)
 # login = LoginManager(app)
 # login.login_view = 'login'
 
+app = Flask(__name__)
+wsgi_app = app.wsgi_app
 app.logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.INFO)
