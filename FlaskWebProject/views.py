702517dc5456a0a7c6510646e2fae742886204c6
longlc3 <long18161245@gmail.com>
2022-02-11 11:29:27 +0700

    update view.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -62,14 +62,16 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        app.logger.info('sucessfully login')
+        app.logger('sucessfully login')
+        print('hello you are success')
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or passwords.')
-            app.logger.unsucessfully('unsuccessfully login')
+            app.logger('unsuccessfully login')
+            print('hello you are fail')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
